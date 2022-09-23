9f1fd42d9727cf3749647dbf65881aa8a80f24fc
Jessica Ambrocio <ambrojessica0@gmail.com>
2022-09-23 16:20:58 -0600

    text done for Hero section, needs styling or Jarallax

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <h1>Web studio of effective development</h1>
+      <div>
+        <p>We build websites, tools and applications that offer beautiful web presence and ultimate user experience.</p>
+        <a href='#'>Let's partner</a>
+        <a href='#'>View Case Studies</a>
+      </div>
+      <ul>
+        <li>Full spectrum of services</li>
+        <li>Flexible work terms</li>
+      </ul>
+    </div>
+  )
+}
